c50ebcb8281d83ed348bfe1d1109ae01648b2b5c
Collin Adler <collinadler@gmail.com>
2014-10-23 01:58:39 -0500

    Completed simple data types and objects

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -11,18 +11,42 @@
 @implementation NumberHandler
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
-    /* WORK HERE */
-    return @0;
+    
+    // I suspect there is a shorthand way to figure this problem out
+    int intNumber = [number intValue];
+    intNumber = intNumber * 2;
+    NSNumber *doubleNumber = [NSNumber numberWithInt:intNumber];
+    
+    return doubleNumber;
 }
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
-    /* WORK HERE */
-    return @[];
+    
+    NSInteger small = MIN(number, otherNumber);
+    NSInteger large = MAX(number, otherNumber);
+    NSMutableArray *numberArray = [[NSMutableArray alloc] init];
+    NSLog(@"%ld, %ld", (long)small, (long)large);
+    
+    for (small; small <= large; small++) {
+        NSNumber *numberForArray = [NSNumber numberWithInteger:small];
+        [numberArray addObject:numberForArray];
+        NSLog(@"%@", numberForArray);
+    }
+    return numberArray;
 }
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
-    /* WORK HERE */
-    return 0;
+
+    NSNumber *arrayNumber = [arrayOfNumbers firstObject];
+    NSInteger finalNumber = [arrayNumber integerValue];
+    
+    for (NSNumber *number in arrayOfNumbers) {
+        int testNumber = [number intValue];
+        if (testNumber < finalNumber) {
+            finalNumber = testNumber;
+        }
+    }
+    return finalNumber;
 }
 
 @end
